e387752fa9f6c85b3741f563fe066af3f7bd428b
SPie <d.spie@pm.me>
2019-04-16 22:34:27 +0200

    made LaravelServiceProvider and LumenServiceProvider classes final; added illuminate/config requirement

--- tests/Unit/LumenServiceProviderTest.php
+++ tests/Unit/LumenServiceProviderTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Auth\AuthManager;
+use Illuminate\Config\Repository;
 use Illuminate\Contracts\Foundation\Application;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
 use SPie\LaravelJWT\Providers\LumenServiceProvider;
 
@@ -19,41 +22,98 @@ final class LumenServiceProviderTest extends TestCase
      */
     public function testBoot(): void
     {
-        $app = Mockery::spy(Application::class);
+        $configRepository = $this->createConfigRepository();
+        $configRepository
+            ->shouldReceive('get')
+            ->andReturn([]);
+        $app = $this->createApp($configRepository);
+        $lumenServiceProvider = $this->createLumenServiceProvider($app);
 
-        $lumenServiceProvider = Mockery::spy(LumenServiceProvider::class, [$app]);
-        $lumenServiceProvider
-            ->makePartial()
-            ->shouldAllowMockingProtectedMethods();
-        $lumenServiceProvider
-            ->shouldReceive('mergeConfigFrom')
-            ->andReturnNull();
-        $lumenServiceProvider
-            ->shouldReceive('extendAuthGuard')
-            ->andReturn($lumenServiceProvider);
+        $this->assertEmpty($lumenServiceProvider->boot());
 
-        $lumenServiceProvider->boot();
-
+        $configRepository
+            ->shouldHaveReceived('set')
+            ->with(
+                'jwt',
+                Mockery::on(function ($argument) {
+                    return \is_array($argument);
+                })
+            )
+            ->once();
+        $configRepository
+            ->shouldHaveReceived('get')
+            ->with(
+                'jwt',
+                []
+            )
+            ->once();
         $app
             ->shouldHaveReceived('configure')
             ->with('jwt')
             ->once();
+    }
 
-        $lumenServiceProvider
-            ->shouldHaveReceived('mergeConfigFrom')
-            ->with(
-                Mockery::on(function ($argument) {
-                    return (\preg_match('/(\/config\/jwt\.php)/', $argument, $matched) == 1);
-                }),
-                'jwt'
-            )
-            ->once();
+    //endregion
 
-        $lumenServiceProvider
-            ->shouldHaveReceived('extendAuthGuard')
-            ->once();
+    //region Mocks
 
-        $this->assertTrue(true);
+    /**
+     * @param Application|null $app
+     *
+     * @return LumenServiceProvider|MockInterface
+     */
+    private function createLumenServiceProvider(Application $app = null): LumenServiceProvider
+    {
+        return new LumenServiceProvider($app ?: $this->createApp());
+    }
+
+    /**
+     * @param Repository|null  $configRepository
+     * @param AuthManager|null $authManager
+     *
+     * @return Application|MockInterface
+     */
+    private function createApp(Repository $configRepository = null, AuthManager $authManager = null): Application
+    {
+        $app = Mockery::spy(Application::class, ArrayAccess::class);
+        $app
+            ->shouldReceive('offsetGet')
+            ->andReturnUsing(function ($argument) use ($configRepository) {
+                switch ($argument) {
+                    case 'config':
+                        return $configRepository ?: $this->createConfigRepository();
+                    default:
+                        return null;
+                }
+            });
+        $app
+            ->shouldReceive('get')
+            ->andReturnUsing(function ($argument) use ($authManager) {
+                switch ($argument) {
+                    case 'auth':
+                        return $authManager ?: $this->createAuthManager();
+                    default:
+                        return null;
+                }
+            });
+
+        return $app;
+    }
+
+    /**
+     * @return Repository|MockInterface
+     */
+    private function createConfigRepository(): Repository
+    {
+        return Mockery::spy(Repository::class);
+    }
+
+    /**
+     * @return AuthManager|MockInterface
+     */
+    private function createAuthManager(): AuthManager
+    {
+        return Mockery::spy(AuthManager::class);
     }
 
     //endregion
